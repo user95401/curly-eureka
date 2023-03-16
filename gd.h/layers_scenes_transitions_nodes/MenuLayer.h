ba3b388c5d6cdaf5da8ecadd619629455f21e8ed
user95401 <user95401@gmail.com>
2023-03-16 09:42:26 +0400

    buttons in menulayer
    https://github.com/geode-sdk/geode/blob/main/bindings/GeometryDash.bro#L3995

--- gd.h/layers_scenes_transitions_nodes/MenuLayer.h
+++ gd.h/layers_scenes_transitions_nodes/MenuLayer.h
@@ -30,6 +30,52 @@ namespace gd {
 			// thx pie
 			reinterpret_cast<void(__thiscall*)(char*, void*, bool)>(base + 0x192150)(reinterpret_cast<char*>(this) + sizeof(cocos2d::CCLayer), alert, btn2);
 		}
+		//g e o d e 💥
+		void onMoreGames(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x1919c0)(this, pSender);
+		}
+		void onGarage(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191dc0)(this, pSender);
+		}
+		void onQuit(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191fa0)(this, pSender);
+		}
+		void onMyProfile(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x1e0950)(this, pSender);
+		}
+		void onPlay(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191b50)(this, pSender);
+		}
+		void onCreator(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191cd0)(this, pSender);
+		}
+		void onRobTop(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191940)(this, pSender);
+		}
+		void onNewgrounds(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191e90)(this, pSender);
+		}
+		void onDaily(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x1916e0)(this, pSender);
+		}
+		void onOptions(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191c30)(this, pSender);
+		}
+		void onAchievements(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191af0)(this, pSender);
+		}
+		void onStats(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191ca0)(this, pSender);
+		}
+		void onFacebook(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191960)(this, pSender);
+		}
+		void onTwitter(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x191980)(this, pSender);
+		}
+		void onYouTube(cocos2d::CCObject* pSender) {
+			reinterpret_cast<void(__thiscall*)(void*, cocos2d::CCObject*)>(base + 0x1919A0)(this, pSender);
+		}
 	};
 }
 
