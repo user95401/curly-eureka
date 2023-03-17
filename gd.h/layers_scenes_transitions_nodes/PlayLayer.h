9611fb9c73c070b1455c89e7c9c9c407a062559e
user95401 <user95401@gmail.com>
2023-03-17 11:55:27 +0400

    Update PlayLayer.h
    some things maybe broken

--- gd.h/layers_scenes_transitions_nodes/PlayLayer.h
+++ gd.h/layers_scenes_transitions_nodes/PlayLayer.h
@@ -203,6 +203,205 @@ namespace gd {
 					base + 0x20d0d0
 				)(this, on);
 			}
+
+			void addObject(GameObject* obj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, GameObject*)>(base + 0x2017e0)(this, obj);
+			}
+			void addToSpeedObjects(GameObject* obj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, GameObject*)>(base + 0x208730)(this, obj);
+			}
+			void animateInDualGround(GameObject* obj, float fl, bool b) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, GameObject*, float, bool)>(base + 0x208b10)(this, obj,fl,b);
+			}
+			void animateInGround(bool b) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool)>(base + 0x209b20)(this, b);
+			}
+			void animateOutGround(bool b) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool)>(base + 0x209e60)(this, b);
+			}
+			void applyEnterEffect(GameObject* obj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, GameObject*)>(base + 0x206880)(this, obj);
+			}
+			void cameraMoveY(float fl1, float fl2, float fl3) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float, float, float)>(base + 0x207c80)(this, fl1, fl2, fl3);
+			}
+			void checkCollisions(PlayerObject* plobj, float fl) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, PlayerObject*, float)>(base + 0x203CD0)(this, plobj, fl);
+			}
+			void circleWaveWillBeRemoved(CCCircleWave* thing) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, CCCircleWave*)>(base + 0x20aa90)(this, thing);
+			}
+			void destroyPlayer(PlayerObject* plrobj, GameObject* obj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, PlayerObject*, GameObject*)>(base + 0x20a1a0)(this, plrobj, obj);
+			}
+			void flipFinished() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20ad10)(this);
+			}
+			virtual void flipGravity(PlayerObject* plrobj, bool bool1, bool bool2) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, PlayerObject*, bool, bool)>(base + 0x207d30)(this, plrobj, bool1, bool2);
+			}
+			void fullReset() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20bdf0)(this);
+			}
+			void getLastCheckpoint() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20b800)(this);
+			}
+			void getObjectsState() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20b8b0)(this);
+			}
+			void hasUniqueCoin(GameObject* obj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, GameObject*)>(base + 0x202250)(this, obj);
+			}
+			void levelComplete() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x1fd3d0)(this);
+			}
+			void loadDefaultColors() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x206e10)(this);
+			}
+			void loadFromCheckpoint(CheckpointObject* checkpoint) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, CheckpointObject*)>(base + 0x20b4a0)(this, checkpoint);
+			}
+			void markCheckpoint() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20b450)(this);
+			}
+			void moveCameraToPos(cocos2d::CCPoint point) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, cocos2d::CCPoint)>(base + 0x207bf0)(this, point);
+			}
+			virtual void onEnterTransitionDidFinish() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20dbd0)(this);
+			}
+			void onExit() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20d810)(this);
+			}
+			void optimizeColorGroups() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x202370)(this);
+			}
+			void optimizeSaveRequiredGroups() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x2026f0)(this);
+			}
+			void pauseGame(bool par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool)>(base + 0x20d3c0)(this, par);
+			}
+			void playAnimationCommand(int int1, int int2) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, int, int)>(base + 0x1ff2f0)(this, int1, int2);
+			}
+			void playExitDualEffect(PlayerObject* plrogj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, PlayerObject*)>(base + 0x208e20)(this, plrogj);
+			}
+			void playFlashEffect(float fl1, int intpar, float fl2) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float, int, float)>(base + 0x75e50)(this, fl1, intpar, fl2);
+			}
+			void playGravityEffect(bool par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool)>(base + 0x207e00)(this, par);
+			}
+			void playSpeedParticle(float par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float)>(base + 0x201130)(this, par);
+			}
+			void playerWillSwitchMode(PlayerObject* plrobj, GameObject* obj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, PlayerObject*, GameObject*)>(base + 0x16b290)(this, plrobj, obj);
+			}
+			void prepareSpawnObjects() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20c850)(this);
+			}
+			void removeAllObjects() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20da00)(this);
+			}
+			void removeLastCheckpoint() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20b830)(this);
+			}
+			void resetLevel() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20bf00)(this);
+			}
+			void resume() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20d5c0)(this);
+			}
+			void resumeAndRestart() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20d4c0)(this);
+			}
+			void saveRecordAction(bool par, PlayerObject* plrobj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool, PlayerObject*)>(base + 0x20ad40)(this, par, plrobj);
+			}
+			void setupLevelStart(LevelSettingsObject* levelSettingsObj) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, LevelSettingsObject*)>(base + 0x1fb780)(this, levelSettingsObj);
+			}
+			void shakeCamera(float fl1, float fl2, float fl3) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float, float, float)>(base + 0x1ff210)(this, fl1, fl2, fl3);
+			}
+			void showCompleteEffect() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x1fe060)(this);
+			}
+			void showCompleteText() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x1fda90)(this);
+			}
+			void showEndLayer() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x1ffd00)(this);
+			}
+			void showNewBest(bool bool1, int int1, int int2, bool bool2, bool bool3, bool bool4) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool, int, int, bool, bool, bool)>(base + 0x1fe3a0)(this,bool1,int1,int2,bool2,bool3,bool4);
+			}
+			void showTwoPlayerGuide() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x2003b0)(this);
+			}
+			void sortGroups() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x202890)(this);
+			}
+			void spawnPlayer2() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x2089e0)(this);
+			}
+			void startMusic() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20C8F0)(this);
+			}
+			void stopCameraShake() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x1ff2d0)(this);
+			}
+			void storeCheckpoint(CheckpointObject* Checkpoint) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, CheckpointObject*)>(base + 0x20b420)(this, Checkpoint);
+			}
+			void timeForXPos(float par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float)>(base + 0x2087d0)(this, par);
+			}
+			void timeForXPos2(float par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float)>(base + 0x1fd3d0)(this, par);
+			}
+			void toggleDualMode(GameObject* obj, bool bool1, PlayerObject* plrobj, bool bool2) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, GameObject*, bool, PlayerObject*, bool)>(base + 0x208880)(this, obj, bool1, plrobj, bool2);
+			}
+			void toggleFlipped(bool bool1, bool bool2) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool, bool)>(base + 0x20ab20)(this, bool1, bool2);
+			}
+			void toggleGlitter(bool par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, bool)>(base + 0x20a0d0)(this, par);
+			}
+			void toggleProgressbar() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x208160)(this);
+			}
+			void updateAttempts() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20ced0)(this);
+			}
+			void updateCamera(float par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float)>(base + 0x2071d0)(this, par);
+			}
+			void updateDualGround(PlayerObject* plrobj, int par1, bool par2) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, PlayerObject*, int, bool)>(base + 0x209680)(this, plrobj, par1, par2);
+			}
+			void updateEffectPositions() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20aab0)(this);
+			}
+			void updateLevelColors() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x203a90)(this);
+			}
+			void updateProgressbar() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x69b40)(this);
+			}
+			virtual void visit() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x200020)(this);
+			}
+			void visitWithColorFlash() {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x200190)(this);
+			}
+			void xPosForTime(float par) {
+				reinterpret_cast<void(__thiscall*)(PlayLayer*, float)>(base + 0x208840)(this, par);
+			}
 	};
 }
 
