0214a8d08e0737df864896b5f16789d16bcb7939
user95401 <user95401@gmail.com>
2023-03-19 22:42:31 +0400

    Update mapped-hooks.hpp

--- mapped-hooks.hpp
+++ mapped-hooks.hpp
@@ -22,3 +22,40 @@ namespace MHook {
     }
 }
 
+//lol 
+#define HOOK_GD(offset, name) create_hook_gd(offset, h_##name, reinterpret_cast<LPVOID*>(&o_##name))
+#define CC_HOOK(symbol, hook, orig) MH_CreateHook((PVOID)((int)GetProcAddress(GetModuleHandle("libcocos2d.dll"), symbol)), hook, (PVOID*)&orig)
+
+static bool create_hook(
+	LPVOID target,
+	LPVOID hook,
+	LPVOID* original)
+{
+	auto hook_status = MH_CreateHook(target, hook, original);
+	if (hook_status != MH_STATUS::MH_OK) return false;
+	return MH_EnableHook(target) == MH_STATUS::MH_OK;
+}
+
+static bool create_hook_gd(
+	const std::uintptr_t offset,
+	LPVOID hook,
+	LPVOID* original)
+{
+	const auto base = reinterpret_cast<std::uintptr_t>(GetModuleHandle(NULL));
+	auto pTarget = reinterpret_cast<LPVOID>(base + offset);
+	return create_hook(pTarget, hook, original);
+}
+
+static bool create_hook_method(
+	const std::string module_name,
+	const std::string signature,
+	LPVOID hook,
+	LPVOID* original)
+{
+	auto h_module = GetModuleHandleA(module_name.c_str());
+	if (!h_module) return false;
+	auto method_address = GetProcAddress(h_module, signature.c_str());
+	if (!method_address) return false;
+	return create_hook(method_address, hook, original);
+}
+
