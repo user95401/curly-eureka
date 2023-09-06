9fa535e73501f141c76eb32614328b04ec9a2145
user95401 <user95401@gmail.com>
2023-09-06 21:55:53 +0400

    Update LoadingLayer.h
    Co-Authored-By: Viral <129858326+viralzah@users.noreply.github.com>

--- gd.h/layers_scenes_transitions_nodes/LoadingLayer.h
+++ gd.h/layers_scenes_transitions_nodes/LoadingLayer.h
@@ -4,35 +4,37 @@
 #include <gd.h>
 
 namespace gd {
-	class GDH_DLL LoadingLayer : public cocos2d::CCLayer {
-	public:
-		PAD(4);
-		int m_nLoadIndex;
-		cocos2d::CCLabelBMFont* m_pCaption;
-		PAD(4);
-		//artifacts of rob debugging something
-		cocos2d::CCSprite* m_pSliderBar;
-		float m_fSliderGrooveXPos;
-		PAD(4);
-		bool m_bFromRefresh;
-		static LoadingLayer* create(bool fromReload) {
-			return reinterpret_cast<LoadingLayer* (__fastcall*)(bool)>(
-				base + 0x18BFE0
-				)(fromReload);
-		}
-		void setFromRefresh(bool value) {
-			m_bFromRefresh = value;
-		}
-		static const char* getLoadingString() {
-			return reinterpret_cast<const char*(__fastcall*)()>(base + 0x18cf40)();
-		}
-		void loadAssets() {
-			reinterpret_cast<void(__thiscall*)(void*)>(base + 0x18C8E0)(this);
-		}
-		void loadingFinished() {
-			reinterpret_cast<void(__thiscall*)(void*)>(base + 0x18C790)(this);
-		}
-	};
+    class GDH_DLL LoadingLayer : public cocos2d::CCLayer {
+    public:
+        PAD(4);
+        int m_nLoadIndex;
+        cocos2d::CCLabelBMFont* m_pCaption;
+        CCNode* m_pLoadingText;
+        cocos2d::CCSprite* m_pSliderBar;
+        float m_fSliderGrooveXPos;
+        PAD(4);
+        bool m_bFromRefresh;
+        static LoadingLayer* create(bool fromReload) {
+            return reinterpret_cast<LoadingLayer * (__fastcall*)(bool)>(
+                base + 0x18BFE0
+                )(fromReload);
+        }
+        void setFromRefresh(bool value) {
+            m_bFromRefresh = value;
+        }
+        bool init() {
+            return reinterpret_cast<bool(__fastcall*)()>(base + 0x18c080)();
+        }
+        static const char* getLoadingString() {
+            return reinterpret_cast<const char* (__fastcall*)()>(base + 0x18cf40)();
+        }
+        void loadAssets() {
+            reinterpret_cast<void(__thiscall*)(void*)>(base + 0x18C8E0)(this);
+        }
+        void loadingFinished() {
+            reinterpret_cast<void(__thiscall*)(void*)>(base + 0x18C790)(this);
+        }
+    };
 }
 
 #endif
