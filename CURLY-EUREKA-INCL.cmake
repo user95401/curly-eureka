1ef6bc838732c4132af479ed8f3bfb325413051d
user95401 <user95401@gmail.com>
2023-09-13 23:09:45 +0400

    13.09.23

---
+++ CURLY-EUREKA-INCL.cmake
@@ -0,0 +1,19 @@
+﻿message("curly-eureka included...\n dir - ${CMAKE_CURRENT_LIST_DIR}")
+
+#curly-eureka
+file(GLOB_RECURSE curly_eureka_src 
+${CMAKE_CURRENT_LIST_DIR}/*.cpp
+${CMAKE_CURRENT_LIST_DIR}/SimpleIni/*
+)
+set(curly_eureka_incl
+${CMAKE_CURRENT_LIST_DIR}
+${CMAKE_CURRENT_LIST_DIR}/gd.h
+${CMAKE_CURRENT_LIST_DIR}/gd.h/include
+${CMAKE_CURRENT_LIST_DIR}/minhook/include
+${CMAKE_CURRENT_LIST_DIR}/SimpleIni
+)
+
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/cocos-headers cocos-headers-build)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/minhook minhook-build)
+include_directories(${curly_eureka_incl})
+link_libraries(minhook cocos2d)
