9876b8501332a1bb247f463aab99d4e515453dcc
user95401 <user95401@gmail.com>
2023-09-18 16:16:25 +0400

    no imgui in cmake

--- CURLY-EUREKA-INCL.cmake
+++ CURLY-EUREKA-INCL.cmake
@@ -13,12 +13,9 @@ ${CMAKE_CURRENT_LIST_DIR}/gd.h/include
 ${CMAKE_CURRENT_LIST_DIR}/minhook/include
 ${CMAKE_CURRENT_LIST_DIR}/SimpleIni
 ${CMAKE_CURRENT_LIST_DIR}/DiscordRPCSDK/Includes
-${CMAKE_CURRENT_LIST_DIR}/imgui-hook
-${CMAKE_CURRENT_LIST_DIR}/imgui-hook/imgui
 )
 message("curly_eureka_incl - ${curly_eureka_incl}")
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/cocos-headers cocos-headers-build)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/minhook minhook-build)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/imgui-hook imgui-hook)
 include_directories(${curly_eureka_incl})
-link_libraries(minhook cocos2d imgui-hook)
+link_libraries(minhook cocos2d)
