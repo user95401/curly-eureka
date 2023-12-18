1cb4b4035d5d50608c59d4ef3dacec0bf2b9a9c7
user95401 <user95401@gmail.com>
2023-12-18 21:24:05 +0400

    idk why

--- CURLY-EUREKA-INCL.cmake
+++ CURLY-EUREKA-INCL.cmake
@@ -1,17 +1,24 @@
-﻿message("curly-eureka included...\n dir - ${CMAKE_CURRENT_LIST_DIR}")
+﻿message("curly-eureka included...\ndir - ${CMAKE_CURRENT_LIST_DIR}")
 
-#curly-eureka
-file(GLOB_RECURSE curly_eureka_src 
+#configure gdh game ver
+if(DEFINED ${GDH_GAME_VERSION})
+    message("NEW GDH_GAME_VERSION FOUND - ${GDH_GAME_VERSION}")
+else()
+    set(GDH_GAME_VERSION "2.113")
+endif()
+
+file(GLOB_RECURSE CURLY_EUREKA_SRC 
 ${CMAKE_CURRENT_LIST_DIR}/*.cpp
 ${CMAKE_CURRENT_LIST_DIR}/SimpleIni/*
 ${CMAKE_CURRENT_LIST_DIR}/DiscordRPCSDK/SourceFiles/*
 ${CMAKE_CURRENT_LIST_DIR}/jute/*
 "${CMAKE_CURRENT_LIST_DIR}/kuba--zip/*"
 )
-set(curly_eureka_incl
+
+set(CURLY_EUREKA_INCL
 ${CMAKE_CURRENT_LIST_DIR}
-${CMAKE_CURRENT_LIST_DIR}/gd.h
-${CMAKE_CURRENT_LIST_DIR}/gd.h/include
+${CMAKE_CURRENT_LIST_DIR}/gd.h_${GDH_GAME_VERSION}
+${CMAKE_CURRENT_LIST_DIR}/gd.h_${GDH_GAME_VERSION}/include
 ${CMAKE_CURRENT_LIST_DIR}/minhook/include
 ${CMAKE_CURRENT_LIST_DIR}/SimpleIni
 ${CMAKE_CURRENT_LIST_DIR}/DiscordRPCSDK/Includes
@@ -21,5 +28,5 @@ ${CMAKE_CURRENT_LIST_DIR}/jute
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/cocos-headers cocos-headers-build)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/minhook minhook-build)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/uibuilder uibuilder-build)
-include_directories(${curly_eureka_incl})
+include_directories(${CURLY_EUREKA_INCL})
 link_libraries(minhook cocos2d)
